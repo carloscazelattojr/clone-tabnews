998ec7b2eba82bbf956f34341b1d4808e05ef8fb
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-09-04 10:09:14 +0000

    config neon database

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,9 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: true,
   });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
